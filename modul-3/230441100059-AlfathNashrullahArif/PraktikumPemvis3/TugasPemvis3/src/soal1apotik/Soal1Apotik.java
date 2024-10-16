e29cd4729099295f794b230d41e00e8709a28f71
ALIF <alfathnaa8@gmail.com>
2024-10-16 22:36:01 +0700

    230441100059-Alfath Nashrullah Arif-Modul 3-Kak Dhani

---
+++ modul-3/230441100059-AlfathNashrullahArif/PraktikumPemvis3/TugasPemvis3/src/soal1apotik/Soal1Apotik.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1apotik;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Soal1Apotik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
